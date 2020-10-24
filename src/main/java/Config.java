84819b332970bc58add5141f0113329e0be39845
Neo Wang <43394822+nwatx@users.noreply.github.com>
2020-10-24 14:31:39 -0500

    Update Config.java

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -4,10 +4,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class Config {
-    protected static final String BOT_TOKEN = "NzIyMjI1NTQ5NzI0MDkwMzY4.Xuf--w.PX9oHuvp1Np1AYCfrZSDjCTm3cM";
-    protected static final String WOLFRAM_API_KEY = "WVE4YV-GQX2VWAU8R";
+    protected static final String BOT_TOKEN = "NzIyMjI1LWQ5NzI0MDkwMzY4.Xuf--w.PX9oHuvp1Np1AYCfrZLKAJSDTm3cM";
+    protected static final String WOLFRAM_API_KEY = "WVE4YV-GQ10921AU8R";
     protected static final String LOGGING_CHANNEL = "723366351971942491";
-    protected static final List<String> op = Collections.singletonList("190992299591729153");
+    protected static final List<String> op = Collections.singletonList("190992099591729153");
     protected static final Color WOLFRAM_COLOR = new Color(173, 216, 230);
     protected static final String PREFIX = "$";
 }
